546e0a86c48aa66a33452c15b9c05ca0c941984c
Chaliana Rolon <chaliana@fb.com>
2018-07-09 15:06:21 -0700

    Initial Commit

---
+++ instaplus/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  instaplus
+//
+//  Created by Chaliana Rolon on 7/9/18.
+//  Copyright © 2018 Chaliana Rolon. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ instaplusTests/instaplusTests.m
@@ -0,0 +1,39 @@
+//
+//  instaplusTests.m
+//  instaplusTests
+//
+//  Created by Chaliana Rolon on 7/9/18.
+//  Copyright © 2018 Chaliana Rolon. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface instaplusTests : XCTestCase
+
+@end
+
+@implementation instaplusTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
